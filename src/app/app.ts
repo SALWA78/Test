fe3720919e2162c48c2cfd3b45a68ddbeb43acff
SALWA78 <salwahamzaoui78@gmail.com>
2025-06-21 20:49:56 +0200

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TodoTest } from '../todoTest/todoTest';
+import { FormsModule } from "@angular/forms";
+import { NgFor } from "@angular/common";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TodoTest],
+  imports: [RouterOutlet,FormsModule,CommonModule,NgFor],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+ 
 })
 export class App {
- 
+  tasks:string[]=[];
+  task:string="";
+  i=0
+  addTask(){
+    
+      this.tasks.push(this.task);
+      this.task="";
+      console.log(this.tasks[this.i])
+      this.i++;
+  }
+  deleteTask(i:number){
+      this.tasks.splice(i,1);
+  }
+  
 }
